2c6edbbba31a31697c6134de9d2539715f713ad8
Clivaly Cardoso <clivalydev@gmail.com>
2024-02-14 10:52:02 -0300

    initial config

---
+++ src/test/kotlin/dev/clivaly/requestcreditsystem/RequestCreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.clivaly.requestcreditsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestCreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
