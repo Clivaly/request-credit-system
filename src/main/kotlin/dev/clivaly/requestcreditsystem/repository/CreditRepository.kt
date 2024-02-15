1289d2d9726053f62ab9861dd0cb4be2771cdc1e
Clivaly Cardoso <clivalydev@gmail.com>
2024-02-15 18:32:41 -0300

    Config Service and Impl

--- src/main/kotlin/dev/clivaly/requestcreditsystem/repository/CreditRepository.kt
+++ src/main/kotlin/dev/clivaly/requestcreditsystem/repository/CreditRepository.kt
@@ -2,8 +2,14 @@ package dev.clivaly.requestcreditsystem.repository
 
 import dev.clivaly.requestcreditsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, Long> {
+interface CreditRepository : JpaRepository<Credit, Long> {
+    fun findByCreditCode(credit: UUID): Credit?
+
+    @Query(value = "SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
 }
---
+++ src/main/kotlin/dev/clivaly/requestcreditsystem/service/impl/CustomerService.kt
@@ -0,0 +1,18 @@
+package dev.clivaly.requestcreditsystem.service.impl
+
+import dev.clivaly.requestcreditsystem.entity.Customer
+import dev.clivaly.requestcreditsystem.repository.CustomerRepository
+import dev.clivaly.requestcreditsystem.service.ICustomerService
+import org.springframework.stereotype.Service
+
+@Service
+class CustomerService(private val customerRepository: CustomerRepository) : ICustomerService {
+    override fun save(customer: Customer): Customer =
+        this.customerRepository.save(customer)
+
+    override fun findById(id: Long): Customer = this.customerRepository.findById(id).orElseThrow {
+        throw RuntimeException("Id $id not found")
+    }
+
+    override fun delete(id: Long) = this.customerRepository.deleteById(id)
+}
